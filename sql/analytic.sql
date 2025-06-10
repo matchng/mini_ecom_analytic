78b1a12ad0735b4fc2de2bb663fd8d1a645c1c9d
matchng <matchnng@gmail.com>
2025-06-10 07:33:59 +0000

    change column name of total_sales to sales_amount

--- sql/analytic.sql
+++ sql/analytic.sql
@@ -1,7 +1,7 @@
 -- Monthly Revenue Trend
 SELECT 
 	DATE_TRUNC('month',order_date) as sale_month,
-	SUM(total_sales) as monthly_revenue
+	SUM(sales_amount) as monthly_sales
 FROM fact_orders
 GROUP BY sale_month
 ORDER BY sale_month;
@@ -14,8 +14,8 @@ SELECT
 	fo.order_date,
 	fo.product_price,
 	fo.quantity,
-	fo.total_sales,
-	SUM (fo.total_sales) OVER(PARTITION BY dp.product_id ORDER BY fo.order_date, fo.order_item_id) as running_total
+	fo.sales_amount,
+	SUM (fo.sales_amount) OVER(PARTITION BY dp.product_id ORDER BY fo.order_date, fo.order_item_id) as running_total
 FROM fact_orders fo
 JOIN dim_products dp ON fo.product_id = dp.product_id;
 
@@ -23,11 +23,11 @@ JOIN dim_products dp ON fo.product_id = dp.product_id;
 SELECT
 	p.product_id,
 	p.product_name,
-	SUM(fo.total_sales) as total_revenue
+	SUM(fo.sales_amount) as total_sales
 FROM dim_products p JOIN fact_orders fo
 	ON p.product_id = fo.product_id
 GROUP BY p.product_id,p.product_name
-ORDER BY total_revenue DESC
+ORDER BY total_sales DESC
 LIMIT 5;
 
 --Top 5 Products per Region
@@ -35,8 +35,8 @@ WITH ranked_region_products AS (
 	SELECT 
 		p.product_name,
 		c.region,
-		SUM(fo.total_sales) as total_revenue,
-		DENSE_RANK() OVER(PARTITION BY c.region ORDER BY SUM(fo.total_sales) desc) as ranked_product
+		SUM(fo.sales_amount) as total_sales,
+		DENSE_RANK() OVER(PARTITION BY c.region ORDER BY SUM(fo.sales_amount) desc) as ranked_product
 	FROM dim_products p JOIN fact_orders fo 
 		ON p.product_id = fo.product_id
 	JOIN dim_customers c
@@ -47,9 +47,31 @@ WITH ranked_region_products AS (
 SELECT 
 	product_name,
 	region,
-	total_revenue,
+	total_sales,
 	ranked_product
 FROM ranked_region_products
 WHERE ranked_product BETWEEN 1 and 5
 ORDER BY region, ranked_product;
 
+-- Cumulative Revenue 
+SELECT
+  order_date,
+  SUM(sales_amount) AS daily_sales,
+  SUM(SUM(sales_amount)) OVER (ORDER BY order_date) AS running_total
+FROM fact_orders
+GROUP BY order_date
+ORDER BY order_date;
+
+-- Customer Revenue Contribution Breakdown
+SELECT 
+  c.customer_id, 
+  c.name,
+  COUNT(DISTINCT fo.order_id) AS unique_orders,
+  COUNT(fo.order_item_id) AS total_order_items,
+  COALESCE(SUM(fo.sales_amount),0) AS total_sales,
+  SUM(SUM(fo.sales_amount)) OVER () AS total_revenue,
+  COALESCE(SUM(sales_amount) / SUM(SUM(sales_amount)) OVER (),0) AS percentage_of_total
+FROM customers c 
+LEFT JOIN fact_orders fo USING (customer_id)
+GROUP BY c.customer_id, c.name
+ORDER BY total_sales DESC;
