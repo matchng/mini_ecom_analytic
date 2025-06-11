4ab8f3848123d632631ca9c9e137e1fedeef0abd
matchng <matchnng@gmail.com>
2025-06-11 15:56:25 +0000

    mount data file inside container & change load_data script to relative path

--- sql/load_data.sql
+++ sql/load_data.sql
@@ -1,5 +1,5 @@
 -- sql/load_data.sql
-COPY customers    FROM '/workspaces/mini_ecom_analytic/data/customers.csv'    DELIMITER ',' CSV HEADER;
-COPY products     FROM '/workspaces/mini_ecom_analytic/data/products.csv'     DELIMITER ',' CSV HEADER;
-COPY orders       FROM '/workspaces/mini_ecom_analytic/data/orders.csv'       DELIMITER ',' CSV HEADER;
-COPY order_items  FROM '/workspaces/mini_ecom_analytic/data/order_items.csv'  DELIMITER ',' CSV HEADER;
+COPY customers    FROM '/data/customers.csv'    DELIMITER ',' CSV HEADER;
+COPY products     FROM '/data/products.csv'     DELIMITER ',' CSV HEADER;
+COPY orders       FROM '/data/orders.csv'       DELIMITER ',' CSV HEADER;
+COPY order_items  FROM '/data/order_items.csv'  DELIMITER ',' CSV HEADER;
